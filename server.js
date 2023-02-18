d56c9fa7447be33a05919dbe1b7023bbefa3d8ba
Urs-Praveen <work.erpraveen@gmail.com>
2023-02-18 19:42:38 +0530

    read from file functionality nodeJs and display to screen

--- server.js
+++ server.js
@@ -1,44 +1,48 @@
 
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
    const url = req.url
-   
-   if(url === '/home'){
-        res.setHeader('Content-Type','text/html')
-        res.write('<html>')
-        res.write('<head><title>This is My webpage</title></head>')
-        res.write('<body><h1>Welcome Home<h1>')
-        res.write('<form action="/about" method="POST"><input type="text"><button type"submit">go to about</button></form></body>')
-        res.write('<form action="/node" method="POST"><input type="text"><button type"submit">go to node</button></form></body>')
-        res.write('<form action="/home" method="POST"><input type="text"><button type"submit">go to Home</button></form></body>')
-
-        res.write('</html>')
-        res.end()
-        //process.exit()
-   }else if(url === '/about'){
-            res.setHeader('Content-Type','text/html')
+   const method = req.method;
+   if(url === '/'){
+        fs.readFile("message.txt",{encoding:'utf-8'},(err,data)=>{
+            console.log(err)
             res.write('<html>')
             res.write('<head><title>This is My webpage</title></head>')
-            res.write('<body><h1>Welcome to About Us page<h1>')
-            res.write('<form action="/about" method="POST"><input type="text"><button type"submit">go to about</button></form></body>')
-             res.write('<form action="/node" method="POST"><input type="text"><button type"submit">go to node</button></form></body>')
-             res.write('<form action="/home" method="POST"><input type="text"><button type"submit">go to home</button></form></body>')
+        
+            res.write('<body><h1>Welcome Home<h1>')
+            res.write(`<span>${data}<span>`)
+            res.write('<form action="/message" method="POST"><input type="text" name = "message"><button type"submit">go to about</button></form></body>')
             res.write('</html>')
-            res.end()
-   }else if(url === '/node'){
-    res.setHeader('Content-Type','text/html')
+            return res.end()
+         //process.exit()
+    })
+   
+} else if(url === '/message' && method==='POST'){
+        const body =[]
+        req.on('data',chunk =>{
+            body.push(chunk)
+        })
+        return req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message,err =>{
+                console.log(err)
+                res.statusCode = 302;
+                res.setHeader('Location','/')
+                return res.end()
+            })
+        })
+    }
+    else{
+        res.setHeader('Content-Type','text/html')
     res.write('<html>')
     res.write('<head><title>This is My webpage</title></head>')
-    res.write('<body><h1>Welcome to my Node Js project<h1>')
-    res.write('<form action="/node" method="POST"><input type="text"><button type"submit">go to node</button></form></body>')
-    res.write('<form action="/home" method="POST"><input type="text"><button type"submit">go to home</button></form></body>')
-    res.write('<form action="/about" method="POST"><input type="text"><button type"submit">go to about</button></form></body>')
-    res.write('</html>')
+    res.write('<body><h1>Welcome Home<h1>')
     res.write('</html>')
     res.end()
-   }
-
+    }
 })
 server.listen(4000)
 
